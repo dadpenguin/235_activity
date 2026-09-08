56f004e33f95602a22141ba40bca1bca9a32d6a0
nwea918 <nwea918@aucklanduni.ac.nz>
2026-09-08 21:33:41 +1200

    finished search page and track detail page

--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,8 @@
 from music import create_app
 from music.adapters.memory_repository import MemoryRepository, populate
 from flask import Flask, render_template, redirect, url_for, request
+from music.domainmodel.favourite import Favourite
+from music.domainmodel.user import User
 
 app = create_app()
 repo = MemoryRepository()
@@ -15,6 +17,13 @@ def index():
 @app.route('/track/<int:track_id>')
 def track_detail(track_id: int):
     track = repo.get_track(track_id)
+    favourited = False
+
+    for favourite in repo.get_favourites_by_user("Steve"):
+        if favourite.track.track_id == track_id:
+            favourited = True
+            break
+
     total_rating = 0
     average_rating = 0
     if not track:
@@ -24,7 +33,7 @@ def track_detail(track_id: int):
     for review in reviews:
         total_rating += review.rating
         average_rating = total_rating / len(reviews)
-    return render_template('track_detail.html', track=track, reviews=reviews, average_rating=average_rating)
+    return render_template('track_detail.html', track=track, reviews=reviews, average_rating=average_rating, track_id = track_id, favourited=favourited)
 
 
 @app.route('/search/<type>/<keyword>')
@@ -76,6 +85,28 @@ def search(type: str, keyword: str):
 
 
     return render_template('search.html', results = results, keyword = keyword)
+
+@app.route('/favourite/<int:track_id>')
+def favourite(track_id: int):
+
+    new_track = repo.get_track(track_id)
+    new_user = User(user_id = 300, user_name = 'Steve', password = 'password123')
+    repo.add_user(new_user)
+    favourite = Favourite(favourite_id = track_id, user = new_user, track = new_track)
+    repo.add_favourite(favourite)
+
+    return redirect(url_for('track_detail', track_id = track_id))
+
+@app.route('/unfavourite/<int:track_id>')
+def unfavourite(track_id: int):
+
+    for favourite in repo.get_favourites_by_user("Steve"):
+        if favourite.track.track_id == track_id:
+            not_favourite = Favourite(favourite_id=track_id, user = "Steve", track = repo.get_track(track_id))
+
+    repo.remove_favourite(not_favourite)
+
+    return redirect(url_for('track_detail', track_id = track_id))
 
 if __name__ == "__main__":
     app.run(host='localhost', port=5000, threaded=False, Debug = True)
