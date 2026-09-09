6e691948355ccca967689e85b8606343de25a9c9
dadpenguin <franklinbula.100@gmail.com>
2026-09-09 16:59:21 +1200

    updated browse to root blueprint, abstracted search route, and updated home

--- music/browse/__init__.py
+++
@@ -1,3 +0,0 @@
-from flask import Blueprint
-
-root_blueprint = Blueprint('root', __name__)
--- music/search/search.py
+++ music/search/search.py
@@ -1,63 +1,35 @@
 from flask import Flask, render_template, redirect, url_for, request, Blueprint
+from flask.helpers import abort
+from flask.json import jsonify
 
 import music.adapters.repository as repo
 from music.adapters.memory_repository import MemoryRepository, populate
 from music.domainmodel.favourite import Favourite
 from music.domainmodel.user import User
+from music.search.services import SearchService
 
 search_blueprint = Blueprint(
     'search_bp', __name__, url_prefix='/search'
 )
 
 
-@search_blueprint.route('/<type>/<keyword>')
-def search(type: str, keyword: str):
-    if request.args.get('query') != None:
-        keyword = request.args.get('query')
-        return redirect(url_for('search_bp.search', type=type, keyword=keyword))
+@search_blueprint.route('/')
+def search():
 
-    results_track = []
-    results_genre = []
-    results_album = []
-    results_artist = []
+    query = request.args.get('q', '').strip()
+    search_type = request.args.get('type', '').strip()
 
-    if type not in ['all', 'title', 'album', 'genre', 'artist']:
-        return redirect(url_for('search_bp.search', type='all', keyword='track'))
+    results = SearchService.getAllTracks(query, search_type)
 
-    if type == 'all':
-        for track in repo.repo_instance.get_tracks():
-            if track.title and keyword.lower() in track.title.lower() and track not in results_track:
-                results_track.append(track)
+    is_empty = False
 
-            for genre in track.genres:
-                if genre.name and keyword.lower() in genre.name.lower() and genre not in results_genre:
-                    results_genre.append(genre)
+    has_results = results and any(results.values())
+    if not has_results:
+        is_empty = True
 
-            if track.album and keyword.lower() in track.album.title.lower() and track.album not in results_album:
-                results_album.append(track.album)
+    tracks = results.get("track") or []
+    artists = results.get("artist") or []
+    genres = results.get("genre") or []
+    albums = results.get("album") or []
 
-            if track.artist and keyword.lower() in track.artist.full_name.lower() and track.artist not in results_artist:
-                results_artist.append(track.artist)
-
-    elif type == 'title':
-        for track in repo.repo_instance.get_tracks():
-            if track.title and keyword.lower() in track.title.lower() and track not in results_track:
-                results_track.append(track)
-
-    elif type == 'album':
-        for track in repo.repo_instance.get_tracks():
-            if track.album and keyword.lower() in track.album.title.lower() and track.album not in results_album:
-                results_album.append(track.album)
-
-    elif type == 'artist':
-        for track in repo.repo_instance.get_tracks():
-            if track.artist and keyword.lower() in track.artist.full_name.lower() and track.artist not in results_artist:
-                results_artist.append(track.artist)
-
-    elif type == 'genre':
-        for track in repo.repo_instance.get_tracks():
-            for genre in track.genres:
-                if genre and keyword.lower() in genre.name.lower() and genre not in results_genre:
-                    results_genre.append(genre)
-
-    return render_template('search.html', results_track=results_track, results_artist=results_artist, results_genre=results_genre, results_album=results_album, keyword=keyword)
+    return render_template('search.html', results_track=tracks, results_artist=artists, results_genre=genres, results_album=albums, keyword=query, is_empty=is_empty)
