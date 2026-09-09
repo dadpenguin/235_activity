343571af21347359e4d6221a2213571a10cc51f1
nwea918 <nwea918@aucklanduni.ac.nz>
2026-09-09 13:06:42 +1200

    changed favourite routing blueprints

--- music/favourite/favourite.py
+++ music/favourite/favourite.py
@@ -8,10 +8,6 @@ favourite_blueprint = Blueprint(
     "favourite_bp", __name__, url_prefix="/favourite"
 )
 
-unfavourite_blueprint = Blueprint(
-    "unfavourite_bp", __name__, url_prefix="/unfavourite"
-)
-
 
 @favourite_blueprint.route('/<int:track_id>')
 def favourite(track_id: int):
@@ -23,7 +19,7 @@ def favourite(track_id: int):
 
     return redirect(url_for('track_detail_bp.track_detail', track_id=track_id))
 
-@unfavourite_blueprint.route('/<int:track_id>')
+@favourite_blueprint.route('/remove/<int:track_id>')
 def unfavourite(track_id: int):
 
     for favourite in repo.repo_instance.get_favourites_by_user("Steve"):
