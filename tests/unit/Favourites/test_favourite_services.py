7be1427333dbfb566ad5d5b92555a722b3565abc
hali913-pixel <hali913@aucklanduni.ac.nz>
2026-09-10 18:56:40 +1200

    Add tests for favourites and reviews and utils/user

---
+++ tests/unit/Favourites/test_favourite_services.py
@@ -0,0 +1,46 @@
+from music.adapters.memory_repository import MemoryRepository
+from music.domainmodel.track import Track
+from music.domainmodel.user import User
+from music.favourite.services import FavouriteService
+
+
+def create_repo():
+    repo = MemoryRepository()
+
+    user = User(1, "hameed", "password123")
+    track = Track(10, "Test Track")
+
+    repo.add_user(user)
+    repo.add_track(track)
+
+    return repo
+
+
+def test_add_favourite():
+    repo = create_repo()
+
+    result = FavouriteService.register_favourite(
+        "hameed",
+        10,
+        repo
+    )
+
+    assert result is True
+
+
+def test_remove_favourite():
+    repo = create_repo()
+
+    FavouriteService.register_favourite(
+        "hameed",
+        10,
+        repo
+    )
+
+    result = FavouriteService.remove_favourite(
+        "hameed",
+        10,
+        repo
+    )
+
+    assert result is True
